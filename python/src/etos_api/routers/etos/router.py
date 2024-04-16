e43d29442da57a5ae61512226514b633add8577d
Fredrik Fristedt <fredjn@axis.com>
2024-04-16 15:06:02 +0200

    Add OTEL events for suite validation failures

--- python/src/etos_api/routers/etos/router.py
+++ python/src/etos_api/routers/etos/router.py
@@ -23,6 +23,7 @@ from etos_lib import ETOS
 from fastapi import APIRouter, HTTPException
 from kubernetes import client
 from opentelemetry import trace
+from opentelemetry.trace import Status, StatusCode
 
 from etos_api.library.environment import Configuration, configure_testrun
 from etos_api.library.utilities import sync_to_async
@@ -38,7 +39,7 @@ LOGGER = logging.getLogger(__name__)
 logging.getLogger("pika").setLevel(logging.WARNING)
 
 
-async def validate_suite(test_suite_url: str) -> None:
+async def validate_suite(test_suite_url: str, span: "Span") -> None:
     """Validate the ETOS test suite through the SuiteValidator.
 
     :param test_suite_url: The URL to the test suite to validate.
@@ -48,6 +49,9 @@ async def validate_suite(test_suite_url: str) -> None:
     except AssertionError as exception:
         LOGGER.error("Test suite validation failed!")
         LOGGER.error(exception)
+        span.add_event("Test suite validation failed")
+        span.set_status(Status(StatusCode.ERROR, "Test suite validation failed"))
+        span.record_exception(exception)
         raise HTTPException(
             status_code=400, detail=f"Test suite validation failed. {exception}"
         ) from exception
@@ -65,7 +69,7 @@ async def _start(etos: StartEtosRequest, span: "Span") -> dict:
     span.set_attribute("etos.id", tercc.meta.event_id)
 
     LOGGER.info("Validating test suite.")
-    await validate_suite(etos.test_suite_url)
+    await validate_suite(etos.test_suite_url, span)
     LOGGER.info("Test suite validated.")
 
     etos_library = ETOS("ETOS API", os.getenv("HOSTNAME"), "ETOS API")
